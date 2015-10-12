e814e6656c088ce91b134ab51104d61f74055ff0
ssergii <sergii.salata@gmail.com>
2015-10-12 15:16:41 +0200

    commit message from Katia :(

---
+++ Goodwill.Core/Goodwill.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Goodwill.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.min.js",
+                      "~/Scripts/ui-bootstrap-0.14.0.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                      "~/Scripts/App/app.js",
+                      "~/Scripts/App/Controllers/startGame.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ Goodwill.Core/Goodwill.Web/Scripts/App/app.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('goodwill', [
+        // Angular modules 
+        
+        // Custom modules 
+
+        // 3rd Party Modules
+        
+    ]);
+})();
